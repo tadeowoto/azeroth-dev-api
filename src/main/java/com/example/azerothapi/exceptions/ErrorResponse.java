8021b402b32a898da46f264b74a5722b56c900bb
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-02-26 11:09:19 -0300

    package exeptions added, and ErrorResponse class

---
+++ src/main/java/com/example/azerothapi/exceptions/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.example.azerothapi.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String message, errorDetails;
+    private int statusCode;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(int statusCode, String errorDetails, String message) {
+        this.statusCode = statusCode;
+        this.errorDetails = errorDetails;
+        this.message = message;
+    }
+}
